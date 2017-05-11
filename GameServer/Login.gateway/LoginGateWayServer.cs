fdc20ebd76d116328f38ca076ae0da2ec1ddc21e
shiliupingGithub <582441842@qq.com>
2017-05-11 17:26:06 +0800

    AASDASDSAD

--- GameServer/Login.gateway/LoginGateWayServer.cs
+++ GameServer/Login.gateway/LoginGateWayServer.cs
@@ -1,10 +1,11 @@
 ﻿using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Protocol;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-public  class LoginGateWayServer : AppServer<LoginSession>
+public  class LoginGateWayServer : AppServer<LoginSession, BinaryRequestInfo>
 {
 }
 
